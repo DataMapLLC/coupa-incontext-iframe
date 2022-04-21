904212da5c72abb1a372070a7ef36f6ac0ceafa3
mdavidquinn <59569620+mdavidquinn@users.noreply.github.com>
2022-04-21 07:48:17 -0500

    debugging

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
+    <div>
+      <div className="Context">Yo left text</div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p> */}
-        <br></br>
-        <br></br>
-        <a
-          className="App-link"
-          href="https://www.datamapllc.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn About DataMap
-        </a>
-      </header>
+          <br></br>
+          <br></br>
+          <a
+            className="App-link"
+            href="https://www.datamapllc.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn About DataMap
+          </a>
+        </header>
+      </div>
     </div>
   );
 }

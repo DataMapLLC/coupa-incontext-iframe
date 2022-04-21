8864408062d73b7305cc5bd57058da4d7e952beb
mdavidquinn <59569620+mdavidquinn@users.noreply.github.com>
2022-04-21 08:18:04 -0500

    reverting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import logo from "./DataMap Logo - icon only.png";
 import "./App.css";
 function App() {
-  const queryParams = new URLSearchParams(window.location.search);
-  const coupahost = queryParams.get("coupahost");
-  const user_id = queryParams.get("user_id");
-  const object_type = queryParams.get("object_type");
-  const object_id = queryParams.get("object_id");
-
   return (
     <div>
-      <div className="Context">coupahost is {coupahost}</div>
-      <div className="Context">user_id is {user_id}</div>
-      <div className="Context">object_type is {object_type}</div>
-      <div className="Context">object_id is {object_id}</div>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
